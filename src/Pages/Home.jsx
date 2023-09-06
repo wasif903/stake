364c0e3b466672142a5b5e07b2c08b249995d25a
wasif903 <wasifmehmood903@gmail.com>
2023-09-06 06:32:52 +0500

    pushing

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <div style={{color:"black"}}>Home awidjawlkdawl</div>;
+}
+
+export default Home;
---
+++ src/routes/Route.jsx
@@ -0,0 +1,10 @@
+import Home from "../Pages/Home";
+
+const routes = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
+
+export default routes;
