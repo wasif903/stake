495886c84fc97072a26c72e27282b9b498e9ce50
wasif903 <wasifmehmood903@gmail.com>
2023-09-06 07:01:58 +0500

    pushing change

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,42 @@
+import { Col, Container, Row } from "react-bootstrap";
+import sec1 from "../assets/home/sec1.jpg";
+import styles from "./home.module.css";
+
 function Home() {
-  return <div className="bg-danger">Home</div>;
+  // const icons = [
+  //     {
+
+  //     }
+  // ]
+
+  return (
+    <>
+      <Container>
+        <Row className={styles.wrapper}>
+          <Col
+            lg="6"
+            className="d-flex row justify-content-center align-items-center"
+          >
+            <div className="text-center">
+              <h2 className="text-white">LOREM IPSUM</h2>
+              <p>
+                Welcome to INFINY, a pinnacle of creativity, elegance, and
+                security in the world of online betting. We pride ourselves on
+                being a trusted affiliate program, welcoming newcomers with open
+                arms to embark on an exhilarating gaming journey.
+              </p>
+              <div className="text-center text-white">Start With :</div>
+            </div>
+          </Col>
+          <Col lg="6">
+            <div className="text-center">
+              <img src={sec1} alt="img" className={styles.section} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
